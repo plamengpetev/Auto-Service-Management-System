7c0b85772fc9be45a73acd5b5d48eec2333c3f9f
plamengpetev <plamen.g.petev@gmail.com>
2025-10-25 16:27:40 +0300

    initial commit

---
+++ src/main/java/com/example/Auto_Service_Management_System/AutoServiceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Auto_Service_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServiceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoServiceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Auto_Service_Management_System/AutoServiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Auto_Service_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
